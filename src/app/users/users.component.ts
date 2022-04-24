9fa5ca34fe877fa5455d47d9d21c1d9be566db56
martabakaj <marta.bakaj@gmail.com>
2022-04-24 14:06:23 +0200

    wyświetlanie avatar a i danych uzytkowników

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../todos/storage.service';
+import { User } from './model/user';
+import { UsersResponse } from './model/users-response';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  public users: User[]=[];
+  constructor(
+    private http: HttpClient, 
+    private storge: StorageService,
+    ) {}
 
   ngOnInit(): void {
+    this.getUsers().subscribe(response =>{
+      this.users = response.data;
+    });
+  }
+  public getUsers() {
+   return this.http.get<UsersResponse>("https:/reqres.in/api/users");
   }
 
 }
